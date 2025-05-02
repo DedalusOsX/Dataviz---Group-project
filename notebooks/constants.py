7754b9154dd0abd337e6fdae28fa26815c3c7baf
Artur Aslanyan <31106393+DedalusOsX@users.noreply.github.com>
2025-05-02 18:15:01 +0400

    some changes

---
+++ notebooks/constants.py
@@ -0,0 +1,7 @@
+default_countries = [
+    "Armenia", "Georgia", "Azerbaijan",
+    "United States", "United Kingdom",
+    "Germany", "Russia", "Japan", "China"
+]
+electricity_cols = ['Bioenergy_Electricity_TWh', 'Solar_Electricity_TWh', 'Wind_Electricity_TWh', 'Hydro_Electricity_TWh','Nuclear_Electricity_TWh', 'Oil_Electricity_TWh','Gas_Electricity_TWh', 'Coal_Electricity_TWh']
+edu_cols = ['Pop_PostSec', 'Pop_Usec', 'Pop_Lsec', 'Pop_Primary', 'Pop_IncompPrim', 'Pop_None']
